fb2f9b6570bfe54bae8fbb14cf01615f28f490f7
luo0412 <1095847440@qq.com>
2019-07-12 09:22:26 +0800

    adjust

---
+++ jdk8-boot2-modules-springall-wuyouzhuguli/ch2-spring-security/59.Spring-Security-SessionManager/src/main/java/cc/mrbird/SecuritySessionApplication.java
@@ -0,0 +1,12 @@
+package cc.mrbird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritySessionApplication.class, args);
+    }
+}
---
+++ jdk8-boot2-modules-yidao620c-bucket/springboot-cxf/src/main/java/com/xncoding/webservice/CxfApplication.java
@@ -0,0 +1,12 @@
+package com.xncoding.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxfApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CxfApplication.class, args);
+    }
+
+}
