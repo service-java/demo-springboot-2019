835ed28e68ee8d27024067dee8b5fa6845332b38
luo0412 <1095847440@qq.com>
2020-11-06 12:27:15 +0800

    [db](adjust)

--- jdk8-boot2-yidao620c-2018-xkcoding-2020@nice/ch1-basics/springall-demo-filter-interceptor@nice/src/main/java/cc/mrbird/config/WebConfig.java
+++ jdk8-boot2-yidao620c-2018-xkcoding-2020@nice/ch1-basics/springall-demo-filter-interceptor@nice/src/main/java/cc/mrbird/config/WebConfig.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
+
     @Bean
     public FilterRegistrationBean timeFilter() {
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
--- jdk8-boot2-yidao620c-2018-xkcoding-2020@nice/ch2-office/xkcoding-demo-email/src/test/java/com/xkcoding/email/PasswordTest.java
+++ jdk8-boot2-yidao620c-2018-xkcoding-2020@nice/ch2-office/xkcoding-demo-email/src/test/java/com/xkcoding/email/PasswordTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @author yangkai.shen
  * @date Created in 2019/8/27 16:15
  */
+
 public class PasswordTest extends SpringBootDemoEmailApplicationTests {
     @Autowired
     private StringEncryptor encryptor;
@@ -19,10 +20,10 @@ public class PasswordTest extends SpringBootDemoEmailApplicationTests {
     /**
      * 生成加密密码
      */
-    @Test
     public void testGeneratePassword() {
         // 你的邮箱密码
         String password = "Just4Test!";
+
         // 加密后的密码(注意：配置上去的时候需要加 ENC(加密密码))
         String encryptPassword = encryptor.encrypt(password);
         String decryptPassword = encryptor.decrypt(encryptPassword);
